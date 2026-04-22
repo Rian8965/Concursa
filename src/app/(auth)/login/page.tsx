bdaede77d779e92f5ddc58a6459b8a962601a744
rianchiefe-pixel <chatgptbrba@gmail.com>
2026-04-22 09:13:22 -0300

    24

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,1 @@
-import { LoginClient } from "./LoginClient";
-import { getBuilderPageContent } from "@/lib/builder/builder-server";
-import { BuilderContent } from "@/components/builder/BuilderContent";
-
-export default async function LoginPage() {
-  const content = await getBuilderPageContent("/login");
-  if (content) return <BuilderContent content={content} />;
-  return <LoginClient />;
-}
+export { LoginClient as default } from "./LoginClient";
